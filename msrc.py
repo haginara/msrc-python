9484e8a8511040b0f2d283862ba64b44ac151eee
jonghak choi <jhchoi@nexon.net>
2020-09-15 15:44:57 -0700

    Update find CVRF with CVE

--- msrc.py
+++ msrc.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from datetime import datetime
 from dataclasses import dataclass, asdict, field
 from typing import Dict, List, Optional
 import os
@@ -81,10 +82,12 @@ class CVRF:
 
     def set_products(self, data):
         products = {}
-        for product in data.get("FullProductName"):
+        for product in data.get("FullProductName", []):
             products[product['ProductID']] = product['Value']
 
-        for branch in data.get("Branch"):
+        for branch in data.get("Branch", []):
+            if not branch.get('Items'):
+                break
             for item in branch['Items'][0]['Items']:
                 products[product['ProductID']] = product['Value']
         return products
@@ -123,6 +126,13 @@ class SecurityUpdate:
     def summary(self):
         return f"{self.DocumentTitle}({self.ID}): {len(self.Cvrf.Vulnerability)}"
 
+    @classmethod
+    def load(cls, filename: str):
+        obj = cls()
+        with open(filename) as f:
+            data = json.load(filename)
+            obj.ID = data['ID'] 
+
     def dump(self, filename: str=None):
         if filename is None:
             filename = f"{self.DocumentTitle.replace(' ', '_')}.json"
@@ -145,7 +155,7 @@ class MSRCApi:
             'Accept': 'application/json',
             'api-key': key,
         }
-        self.params = {'api-version': 2020}
+        self.params = {'api-version': datetime.now().year}
 
     def cvrf(self, id):
         #/cvrf/2016-Jan
@@ -165,11 +175,89 @@ class MSRCApi:
             raise(e)
         return cvrf
 
-    def updates(self):
-        updates_query = f"{self.url}/updates"
+    def get_cvrf_id_for_cve(self, cve: str):
+        url = f"{self.url}/Updates('{cve}')"
+        print(url)
+        response = requests.get(url, headers=self.headers, params=self.params)
+        if response.status_code != 200:
+            id = None
+        data = response.json()
+        id = data['value'][0]['ID']
+        return id
+
+    def get_cvrf_id_for_cve2(self, cve: str):
+        year = cve.split('-')[1]
+        
+        CVRFs = []
+        for cvrf_meta in self.get_cvrf_by_year(year):
+            cvrf = self.get_cvrf_by_id(cvrf_meta['ID'])
+            if cvrf.get('Vulnerability'):
+                for vuln in cvrf.get('Vulnerability'):
+                    if cve == vuln['CVE']:
+                        CVRFs.append(cvrf)
+        print(f"Found CVRF with cve: {len(CVRFs)}")
+        return CVRFs
+
+
+    def get_knowledge_bases_for_cve(self, cve: str):
+        CVRFs = self.get_cvrf_id_for_cve2(cve)
+        if CVRFs is None:
+            print("No CVRF found")
+            return []
+        KBs = []
+        for cvrf in CVRFs:
+            for vuln in cvrf['Vulnerability']:
+                if vuln['CVE'] == cve:
+                    print(f"Matched {cvrf['DocumentTracking']['Identification']['ID']}")
+                    print(f"\twith {vuln['CVE']}")
+                    for kb in vuln['Remediations']:
+                        KBs.append(kb['Description']['Value'])
+        return KBs
+
+    def get_cvrf_by_id(self, cvrf_id):
+        #/cvrf/2016-Jan
+        if not re.match(r"\d{4}\-\w{3}", cvrf_id):
+            raise Exception("ID is not required format: yyyy-M")
+        
+        url = f"{self.url}/cvrf/{cvrf_id}"
+        r = requests.get(url, headers=self.headers, params=self.params)
+        if r.status_code != 200:
+            logger.error(f"Failed to get update: {cvrf_id}")
+            raise Exception
+        try:
+            data = r.json()
+        except Exception as e:
+            logger.error(f"Failed to get CVRF: {id} Error: {e}")
+            raise(e)
+        return data
+
+    def get_cvrf_by_year(self, year):
+        if isinstance(year, int):
+            year = str(year)
+
+        for cvrf in self.get_all_cvrf():
+            if cvrf['ID'].startswith(year):
+                yield cvrf
+
+    def get_all_cvrf(self):
+        updates_query = f"{self.url}/Updates"
         r = requests.get(updates_query, headers=self.headers, params=self.params)
         if r.status_code != 200:
             raise SystemExit("Failed to get updates")
+        data = r.json()
+        values = data['value']
+        for value in values:
+            # Example:
+            # {'ID': '2019-Nov', 'Alias': '2019-Nov', 'DocumentTitle': 'November 2019 Security Updates', 'Severity': None, 'InitialReleaseDate': '2019-11-12T08:00:00Z', 'CurrentReleaseDate': '2020-02-03T08:00:00Z', 'CvrfUrl': 'https://api.msrc.microsoft.com/cvrf/2019-Nov?api-Version=2020'}
+            yield value
+
+    def updates(self):
+        updates_query = f"{self.url}/Updates"
+        r = requests.get(updates_query, headers=self.headers, params=self.params)
+        if r.status_code != 200:
+            raise SystemExit("Failed to get updates")
+        data = r.json()
+        data['value']
         updates = [SecurityUpdate(**update) for update in r.json().get('value')]
         for update in updates:
             print(update.CvrfUrl)
@@ -191,25 +279,39 @@ class MSRCApi:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('id')
+    parser.add_argument('--clear-cache')
     parser.add_argument('-k', '--key',
         help="MSRC Key")
     parser.add_argument('-D', '--dump', action='store_true',
         help="Dump result to file")
-
     options = parser.parse_args()
     key = os.getenv('MSRC_KEY', None) or options.key
     msrc = MSRCApi(key)
+
     updates = []
     if options.id.upper() in ['*', 'ALL']:
-        updates = [update for update in msrc.updates()]
+        msrc.get_all_cvrf()
+        raise SystemExit()
+    elif options.id.upper().startswith('CVE'):
+        kbs = msrc.get_knowledge_bases_for_cve(options.id)
+        if len(kbs) == 0:
+            print("No KBs found")
+            raise SystemExit()
+        kbs = list(set(kbs))
+        for kb in kbs:
+            print("KB" +kb)
+        query = "(" + "|".join([f".*{kb}.*" for kb in kbs]) + ")"
+        print(f"Get Computer name from all machines Applicable Patches matching \"{query}\"")
+            
+        raise SystemExit()
     else:
         updates = [msrc.cvrf(options.id)]
 
     for update in updates:
         print(pprint.pformat(update))
         #for v in update.Vulnerabilities:
-        #    print(v.CVE)
-        for vuln in update.affected(['Windows 10', 'Windows 2016', 'Windows 2008', 'Windows 2019']):
+        #    print('CVE: %s' % v.CVE)
+        for vuln in update.affected(['Windows 7', 'Windows 10', 'Windows 2016', 'Windows 2008', 'Windows 2019']):
             print(f"{vuln.CVE}, {vuln.Title['Value']}")
         if options.dump:
             update.dump()
