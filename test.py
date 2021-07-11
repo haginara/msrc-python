653b75886e194e8cfae4010427d3a2bfd402ba2c
Jonghak Choi <haginara@gmail.com>
2021-07-10 23:07:12 -0700

    Update format

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 # coding: utf-8
 import json
 import msrc
-sample = json.load(open('sample.json'))
-cvrf = msrc.CVRF(sample)
-cvrf.products
-cvrf.vulnerabilites
+import msrc
+
+def test_msrc_cve():
+    sample = json.load(open('sample.json'))
+    cvrf = msrc.CVRF(sample)
+    cvrf.products
+    cvrf.vulnerabilites
