9019324a1693b9e5f8e56d21c8fadf0703749c73
Jonghak Choi <haginara@gmail.com>
2022-09-05 21:52:33 -0700

    Updated the readme and version

--- README.md
+++ README.md
@@ -12,4 +12,7 @@ Requirements
 
 Usage
 -----
+	# Search with CVE
 	python msrc.py CVE-2018-8174
+	# Search with KB
+	python msrc.py KB5014699
--- msrc.py
+++ msrc.py
@@ -5,7 +5,7 @@ import argparse
 
 import requests
 
-__version_info__ = (0, 0, 1)
+__version_info__ = (0, 0, 2)
 __version__ = ".".join(map(str, __version_info__))
 
 
